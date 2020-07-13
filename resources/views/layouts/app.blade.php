226912d7379cf440508a468b1c96edfe1ed24092
Cheick-ahmed <chs.ahmed_pro02@outlook.com>
2020-07-13 13:00:33 +0200

    Light mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,5 +14,6 @@
     <main class="container my-12">
         @yield('content')
     </main>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </body>
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,22 +1,39 @@
-<div class="mb-20 fixed top-0 py-3 w-full z-50 top-0 transition-all duration-700 bg-gray-900" :class=" pinned  ?  'shadow bg-gray-700' : 'bg-gray-900'">
-    <nav class="container">
-        <div class="lg:flex lg:items-center lg:justify-between">
-            <div class="flex items-center justify-between">
-                <nuxt-link :to=" { name : 'index' } " class="tracking-wider  uppercase font-header text-lg font-bold italic">
-                    <img class="h-8 w-auto" src="{{ asset('images/logo.svg') }}" alt="Kounghany">
-                </nuxt-link>
-                <button class="focus:outline-none block lg:hidden relative flex flex-col items-center justify-center h-8 w-8">
-                    <span class="h-1 w-6 rounded bg-orange-500 mb-1 transition-all duration-300"></span>
-                    <span class="h-1 w-6 rounded bg-orange-400 mb-1 transition-all duration-300"></span>
-                    <span class="h-1 w-6 rounded bg-orange-400"></span>
-                </button>
-            </div>
-            <div class="text-gray-200 lg:flex py-4" :class=" mobileNavOpen == true ? 'flex-col' : 'hidden' ">
-                <a href="" class="lg:mx-2 font-medium tracking-wider block text-md py-2 lg:py-3 mb-2 lg:mb-0 px-2 lg:px-4 hover:bg-gray-700 rounded">Événements</a>
-                <a href="" class="lg:mx-2 font-medium tracking-wider block text-md py-2 lg:py-3 mb-2 lg:mb-0 px-2 lg:px-4 hover:bg-gray-700 rounded">Réalisations</a>
-                <a href="" class="lg:mx-2 font-medium tracking-wider block text-md py-2 lg:py-3 mb-2 lg:mb-0 px-2 lg:px-4 hover:bg-gray-700 rounded">A propos</a>
-                <a href="" class="lg:mx-2 font-medium tracking-wider block text-md py-2 lg:py-3 mb-2 lg:mb-0 px-2 lg:px-4 hover:bg-gray-700 rounded">Contact</a>
-            </div>
+  <nav x-data="{ open: false }" class="bg-white shadow mb-20 fixed top-0 py-3 w-full z-50 top-0 transition-all duration-700">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img class="block lg:hidden h-8 w-auto" src="{{ asset('images/logo.svg') }}" alt="Logo">
+            <img class="hidden lg:block h-8 w-auto" src="{{ asset('images/logo.svg') }}" alt="Logo">
         </div>
-    </nav>
+        <div class="ml-auto hidden sm:block">
+            <div class="flex">
+              <a href="#" class="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
+              <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
+              <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
+              <a href="#" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+          </div>
+      </div>
+  </div>
+  <div class="-mr-2 flex sm:hidden">
+      <button @click="open = !open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" x-bind:aria-label="open ? 'Close main menu' : 'Main menu'" aria-label="Main menu" x-bind:aria-expanded="open">
+        <svg x-state:on="Menu open" x-state:off="Menu closed" class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+      <svg x-state:on="Menu open" x-state:off="Menu closed" class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+      </svg>
+  </button>
 </div>
+</div>
+</div>
+
+<div x-description="Mobile menu, toggle classes based on menu state." x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'block': open, 'hidden': !open }" class="sm:hidden hidden">
+  <div class="px-2 pt-2 pb-3">
+    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Dashboard</a>
+    <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Team</a>
+    <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Projects</a>
+    <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
+</div>
+</div>
+</nav>
